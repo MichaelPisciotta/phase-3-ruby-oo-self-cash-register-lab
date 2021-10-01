b47514fd3f9b0f456a7af82b53967e4ea17a3872
MichaelPisciotta <mfp56@miami.edu>
2021-10-01 16:03:57 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
+
+    attr_accessor :total
+    attr_accessor :discount
+
+    def initialize(discount = 20)
+      @total = 0
+      @discount = discount 
+    end
+
+    def total 
+
+    end 
+
+    def add_item 
+      
+    end 
+
+    def apply_discount 
+      
+    end 
+
+    def items 
+      
+    end 
+
+    def void_last_transaction
+
+    end
+  
+  end
 
