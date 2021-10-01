3c235a571540b2dee211c3e09a98cde59b72655b
MichaelPisciotta <mfp56@miami.edu>
2021-10-01 16:45:48 -0400

    last 4 tests remaining

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,32 @@
+require 'pry'
+
 class CashRegister
 
-    attr_accessor :total
-    attr_accessor :discount
+    attr_accessor :total, :discount, :last_transaction 
+    
 
-    def initialize(discount = 20)
+    def initialize(discount = 0)
       @total = 0
       @discount = discount 
     end
 
-    def total 
 
-    end 
-
-    def add_item 
-      
+    def add_item (total, price, quantity = 1)
+      self.last_transaction = price * quantity
+      self.total = self.total + self.last_transaction
     end 
 
     def apply_discount 
       
+      if self.discount > 0
+         discount = (100 - self.discount.to_f) / 100
+         self.total = (self.total * discount).to_i
+        "After the discount, the total comes to $#{self.total}."
+      else
+        "There is no discount to apply."
+      end 
+
+
     end 
 
     def items 
